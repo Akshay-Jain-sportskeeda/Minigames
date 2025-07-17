226408e0a717877973441353b55d191945bea36f
Akshay-Jain-sportskeeda <akshay.jain@sportskeeda.com>
2025-07-17 16:55:43 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/games/cricket/guess_the_stats',
+  // base: '/games/cricket/guess_the_stats',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
